7f303df70aed5271c0d8a1ac9a1aaff7ae01c981
zooloo <zooloo@localServerNode>
2023-11-27 20:37:46 +0200

    add new playbook and new files

---
+++ files/service/users.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+arr=(
+"hd ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINEptDIB+7qOP70WUXPm3qE54xTOHvTND0Fd6FJ1piqq hd@webzilla"
+)
+for index in ${!arr[*]}
+do
+    IFS=' ' read -r -a array <<< ${arr[$index]}
+    LOGIN=${array[0]}
+    KEY="${array[1]} ${array[2]} ${array[3]}"
+    sudo adduser $LOGIN
+    sudo usermod -aG wheel $LOGIN
+    sudo su $LOGIN -c "mkdir /home/$LOGIN/.ssh"
+    sudo su $LOGIN -c "chmod 700 /home/$LOGIN/.ssh"
+    sudo su $LOGIN -c "touch /home/$LOGIN/.ssh/authorized_keys"
+    sudo su $LOGIN -c "chmod 600 /home/$LOGIN/.ssh/authorized_keys"
+    sudo su $LOGIN -c "echo $KEY > /home/$LOGIN/.ssh/authorized_keys"
+    sudo echo "$LOGIN ALL=(ALL) NOPASSWD:ALL" >> /tmp/wzilla-users 
+done
+sudo chown root:root /tmp/wzilla-users
+sudo mv /tmp/wzilla-users /etc/sudoers.d/wzilla-users
+sudo chown root:root /etc/sudoers.d/wzilla-users
+sudo chmod 440 /etc/sudoers.d/wzilla-users
